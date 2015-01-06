62608a7346d6bcec56c1ff741e3d05253208e1e7
Anecdata <Anecdata@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-06 15:58:36 +0000

    removed failed tag, trying again
    git-svn-id: https://plugins.svn.wordpress.org/anecdata-recent-project-photos@1061408 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/anecdata-project-photos-widget.php
+++ trunk/anecdata-project-photos-widget.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Anecdata Recent Project Photos
 Description: Recent photos from your project on <a href="http://www.anecdata.org">Anecdata.org!</a>
-Version: 1.0
+Version: 1.0.1
 Author: Anecdata
 Author URI: http://www.anecdata.org
 License: GPL2
@@ -124,6 +124,8 @@ License: GPL2
 			<label for="<?php echo $this->get_field_id( 'project_id' ); ?>"><?php _e( 'Project ID:' ); ?></label> 
 			<input class="widefat" id="<?php echo $this->get_field_id( 'project_id' ); ?>" name="<?php echo $this->get_field_name( 'project_id' ); ?>" type="text" value="<?php echo esc_attr( $project_id ); ?>" />
 		</p>
+		<p class="description">E.g. "http://www.anecdata.org/projects/view/<strong><u>59</u></strong>".</p>
+		<p class="description">Don't have a project yet? <a href="http://anecdata.org/projects" target = "_blank">Create one now.</a></p>
 		<?php 
 	}
 		
